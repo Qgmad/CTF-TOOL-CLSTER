84452d92d19a6ce38aa44d0bcac3933a7e445507
Prince Kaliraman <42971172+Qgmad@users.noreply.github.com>
2020-08-29 21:26:15 +0530

    Update CLSTER.py

--- CLSTER.py
+++ CLSTER.py
@@ -1,7 +1,8 @@
-
 import requests
+#write the initial part of the link that you don't want to add before the test string in host:
 host = "https://www.pastebin.com/"    
-        
+ 
+#Write the string you want to test for every possible combination of upper and lower case letters without changing their position: 
 str = 'zawew7y7'
 
 def getBit(num, bit):
@@ -18,3 +19,4 @@ for i in range(0,2**len(str)):
    r = requests.get(host + out)
    if r.ok:
         print(host + out)
+#All the validated and working links are shown on the display in live time.
